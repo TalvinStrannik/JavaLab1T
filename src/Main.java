273e3bce19ceaf74e9069afc3881856f6d36fa0a
TalvinStrannik <117950745+TalvinStrannik@users.noreply.github.com>
2022-11-12 20:35:08 +0300

    Will I load this?!

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main
@@ -10,9 +9,10 @@ public class Main
         for(int i=0; i< triangles.length; i++)
         {
             System.out.print((i+1)+": ");
-            triangles[i]=new Triangle(new Point(in.nextInt(), in.nextInt()),
-                    new Point(in.nextInt(), in.nextInt()),
-                    new Point(in.nextInt(), in.nextInt()));
+            triangles[i]=new Triangle(
+                    new Point(in.nextDouble(), in.nextDouble()),
+                    new Point(in.nextDouble(), in.nextDouble()),
+                    new Point(in.nextDouble(), in.nextDouble()));
         }
 
     }
@@ -32,53 +32,52 @@ public class Main
         double minOfIsosceles=Double.MAX_VALUE;
         double minOfRectangular=Double.MAX_VALUE;
         double minOfArbitrary=Double.MAX_VALUE;
-        for(int i=0; i< triangles.length; i++)
+        for(Triangle thisTriangle:triangles)
         {
-            if(triangles[i].typeTriangle==TypeTriangle.EQUILATERAL)
+            if(thisTriangle.getTypeTriangle()==TypeTriangle.EQUILATERAL)
             {
-                if(minOfEquilateral>triangles[i].getArea()) minOfEquilateral=triangles[i].getArea();
+                if(minOfEquilateral>thisTriangle.getArea()) minOfEquilateral=thisTriangle.getArea();
             }
-            else if(triangles[i].typeTriangle==TypeTriangle.ISOSCELES)
+            else if(thisTriangle.getTypeTriangle()==TypeTriangle.ISOSCELES || thisTriangle.isIsosceles())
             {
-                if(minOfIsosceles>triangles[i].getArea()) minOfIsosceles=triangles[i].getArea();
+                if(minOfIsosceles>thisTriangle.getArea()) minOfIsosceles=thisTriangle.getArea();
             }
-            else if(triangles[i].typeTriangle==TypeTriangle.RECTANGULAR)
+            else if(thisTriangle.getTypeTriangle()==TypeTriangle.RECTANGULAR)
             {
-                if(minOfRectangular>triangles[i].getArea()) minOfRectangular=triangles[i].getArea();
+                if(minOfRectangular>thisTriangle.getArea()) minOfRectangular=thisTriangle.getArea();
             }
-            else if(minOfArbitrary>triangles[i].getArea()) minOfArbitrary=triangles[i].getArea();
+            else if(minOfArbitrary>thisTriangle.getArea()) minOfArbitrary=thisTriangle.getArea();
         }
-
-        for(int i=0; i< triangles.length; i++)
+        for(Triangle thisTriangle:triangles)
         {
-            if(triangles[i].typeTriangle==TypeTriangle.EQUILATERAL)
+            if(thisTriangle.getTypeTriangle()==TypeTriangle.EQUILATERAL)
             {
-                if(minOfEquilateral==triangles[i].getArea())
+                if(minOfEquilateral==thisTriangle.getArea())
                 {
                     System.out.println("Minimum Area in Equilateral:");
-                    System.out.println(triangles[i]);
+                    System.out.println(thisTriangle);
                 }
             }
-            else if(triangles[i].typeTriangle==TypeTriangle.ISOSCELES)
+            else if(thisTriangle.getTypeTriangle()==TypeTriangle.ISOSCELES || thisTriangle.isIsosceles())
             {
-                if(minOfIsosceles==triangles[i].getArea())
+                if(minOfIsosceles==thisTriangle.getArea())
                 {
                     System.out.println("Minimum Area in Isosceles:");
-                    System.out.println(triangles[i]);
+                    System.out.println(thisTriangle);
                 }
             }
-            else if(triangles[i].typeTriangle==TypeTriangle.RECTANGULAR)
+            else if(thisTriangle.getTypeTriangle()==TypeTriangle.RECTANGULAR)
             {
-                if(minOfRectangular==triangles[i].getArea())
+                if(minOfRectangular==thisTriangle.getArea())
                 {
                     System.out.println("Minimum Area in Rectangular:");
-                    System.out.println(triangles[i]);
+                    System.out.println(thisTriangle);
                 }
             }
-            else if(minOfArbitrary==triangles[i].getArea())
+            else if(minOfArbitrary==thisTriangle.getArea())
             {
                 System.out.println("Minimum Area in Arbitrary:");
-                System.out.println(triangles[i]);
+                System.out.println(thisTriangle);
             }
         }
     }
@@ -88,53 +87,53 @@ public class Main
         double maxOfIsosceles=0;
         double maxOfRectangular=0;
         double maxOfArbitrary=0;
-        for(int i=0; i< triangles.length; i++)
+        for(Triangle thisTriangle:triangles)
         {
-            if(triangles[i].typeTriangle==TypeTriangle.EQUILATERAL)
+            if(thisTriangle.getTypeTriangle()==TypeTriangle.EQUILATERAL)
             {
-                if(maxOfEquilateral<triangles[i].getArea()) maxOfEquilateral=triangles[i].getArea();
+                if(maxOfEquilateral<thisTriangle.getArea()) maxOfEquilateral=thisTriangle.getArea();
             }
-            else if(triangles[i].typeTriangle==TypeTriangle.ISOSCELES)
+            else if(thisTriangle.getTypeTriangle()==TypeTriangle.ISOSCELES || thisTriangle.isIsosceles())
             {
-                if(maxOfIsosceles<triangles[i].getArea()) maxOfIsosceles=triangles[i].getArea();
+                if(maxOfIsosceles<thisTriangle.getArea()) maxOfIsosceles=thisTriangle.getArea();
             }
-            else if(triangles[i].typeTriangle==TypeTriangle.RECTANGULAR)
+            else if(thisTriangle.getTypeTriangle()==TypeTriangle.RECTANGULAR)
             {
-                if(maxOfRectangular<triangles[i].getArea()) maxOfRectangular=triangles[i].getArea();
+                if(maxOfRectangular<thisTriangle.getArea()) maxOfRectangular=thisTriangle.getArea();
             }
-            else if(maxOfArbitrary<triangles[i].getArea()) maxOfArbitrary=triangles[i].getArea();
+            else if(maxOfArbitrary<thisTriangle.getArea()) maxOfArbitrary=thisTriangle.getArea();
         }
 
-        for(int i=0; i< triangles.length; i++)
+        for(Triangle thisTriangle:triangles)
         {
-            if(triangles[i].typeTriangle==TypeTriangle.EQUILATERAL)
+            if(thisTriangle.getTypeTriangle()==TypeTriangle.EQUILATERAL)
             {
-                if(maxOfEquilateral==triangles[i].getArea())
+                if(maxOfEquilateral==thisTriangle.getArea())
                 {
                     System.out.println("Maximum Area in Equilateral:");
-                    System.out.println(triangles[i]);
+                    System.out.println(thisTriangle);
                 }
             }
-            else if(triangles[i].typeTriangle==TypeTriangle.ISOSCELES)
+            else if(thisTriangle.getTypeTriangle()==TypeTriangle.ISOSCELES || thisTriangle.isIsosceles())
             {
-                if(maxOfIsosceles==triangles[i].getArea())
+                if(maxOfIsosceles==thisTriangle.getArea())
                 {
                     System.out.println("Maximum Area in Isosceles:");
-                    System.out.println(triangles[i]);
+                    System.out.println(thisTriangle);
                 }
             }
-            else if(triangles[i].typeTriangle==TypeTriangle.RECTANGULAR)
+            else if(thisTriangle.getTypeTriangle()==TypeTriangle.RECTANGULAR)
             {
-                if(maxOfRectangular==triangles[i].getArea())
+                if(maxOfRectangular==thisTriangle.getArea())
                 {
                     System.out.println("Maximum Area in Rectangular:");
-                    System.out.println(triangles[i]);
+                    System.out.println(thisTriangle);
                 }
             }
-            else if(maxOfArbitrary==triangles[i].getArea())
+            else if(maxOfArbitrary==thisTriangle.getArea())
             {
                 System.out.println("Maximum Area in Arbitrary:");
-                System.out.println(triangles[i]);
+                System.out.println(thisTriangle);
             }
         }
     }
@@ -144,53 +143,53 @@ public class Main
         double minOfIsosceles=Double.MAX_VALUE;
         double minOfRectangular=Double.MAX_VALUE;
         double minOfArbitrary=Double.MAX_VALUE;
-        for(int i=0; i< triangles.length; i++)
+        for(Triangle thisTriangle:triangles)
         {
-            if(triangles[i].typeTriangle==TypeTriangle.EQUILATERAL)
+            if(thisTriangle.getTypeTriangle()==TypeTriangle.EQUILATERAL)
             {
-                if(minOfEquilateral>triangles[i].getPerimeter()) minOfEquilateral=triangles[i].getArea();
+                if(minOfEquilateral>thisTriangle.getPerimeter()) minOfEquilateral=thisTriangle.getArea();
             }
-            else if(triangles[i].typeTriangle==TypeTriangle.ISOSCELES)
+            else if(thisTriangle.getTypeTriangle()==TypeTriangle.ISOSCELES || thisTriangle.isIsosceles())
             {
-                if(minOfIsosceles>triangles[i].getPerimeter()) minOfIsosceles=triangles[i].getArea();
+                if(minOfIsosceles>thisTriangle.getPerimeter()) minOfIsosceles=thisTriangle.getArea();
             }
-            else if(triangles[i].typeTriangle==TypeTriangle.RECTANGULAR)
+            else if(thisTriangle.getTypeTriangle()==TypeTriangle.RECTANGULAR)
             {
-                if(minOfRectangular>triangles[i].getPerimeter()) minOfRectangular=triangles[i].getArea();
+                if(minOfRectangular>thisTriangle.getPerimeter()) minOfRectangular=thisTriangle.getArea();
             }
-            else if(minOfArbitrary>triangles[i].getPerimeter()) minOfArbitrary=triangles[i].getArea();
+            else if(minOfArbitrary>thisTriangle.getPerimeter()) minOfArbitrary=thisTriangle.getArea();
         }
 
-        for(int i=0; i< triangles.length; i++)
+        for(Triangle thisTriangle:triangles)
         {
-            if(triangles[i].typeTriangle==TypeTriangle.EQUILATERAL)
+            if(thisTriangle.getTypeTriangle()==TypeTriangle.EQUILATERAL)
             {
-                if(minOfEquilateral==triangles[i].getPerimeter())
+                if(minOfEquilateral==thisTriangle.getPerimeter())
                 {
                     System.out.println("Minimum Perimeter in Equilateral:");
-                    System.out.println(triangles[i]);
+                    System.out.println(thisTriangle);
                 }
             }
-            else if(triangles[i].typeTriangle==TypeTriangle.ISOSCELES)
+            else if(thisTriangle.getTypeTriangle()==TypeTriangle.ISOSCELES || thisTriangle.isIsosceles())
             {
-                if(minOfIsosceles==triangles[i].getPerimeter())
+                if(minOfIsosceles==thisTriangle.getPerimeter())
                 {
                     System.out.println("Minimum Perimeter in Isosceles:");
-                    System.out.println(triangles[i]);
+                    System.out.println(thisTriangle);
                 }
             }
-            else if(triangles[i].typeTriangle==TypeTriangle.RECTANGULAR)
+            else if(thisTriangle.getTypeTriangle()==TypeTriangle.RECTANGULAR)
             {
-                if(minOfRectangular==triangles[i].getPerimeter())
+                if(minOfRectangular==thisTriangle.getPerimeter())
                 {
                     System.out.println("Minimum Perimeter in Rectangular:");
-                    System.out.println(triangles[i]);
+                    System.out.println(thisTriangle);
                 }
             }
-            else if(minOfArbitrary==triangles[i].getPerimeter())
+            else if(minOfArbitrary==thisTriangle.getPerimeter())
             {
                 System.out.println("Minimum Perimeter in Arbitrary:");
-                System.out.println(triangles[i]);
+                System.out.println(thisTriangle);
             }
         }
 
@@ -201,106 +200,123 @@ public class Main
         double maxOfIsosceles=0;
         double maxOfRectangular=0;
         double maxOfArbitrary=0;
-        for(int i=0; i< triangles.length; i++)
+        for(Triangle thisTriangle:triangles)
         {
-            if(triangles[i].typeTriangle==TypeTriangle.EQUILATERAL)
+            if(thisTriangle.getTypeTriangle()==TypeTriangle.EQUILATERAL)
             {
-                if(maxOfEquilateral<triangles[i].getPerimeter()) maxOfEquilateral=triangles[i].getArea();
+                if(maxOfEquilateral<thisTriangle.getPerimeter()) maxOfEquilateral=thisTriangle.getArea();
             }
-            else if(triangles[i].typeTriangle==TypeTriangle.ISOSCELES)
+            else if(thisTriangle.getTypeTriangle()==TypeTriangle.ISOSCELES || thisTriangle.isIsosceles())
             {
-                if(maxOfIsosceles<triangles[i].getPerimeter()) maxOfIsosceles=triangles[i].getArea();
+                if(maxOfIsosceles<thisTriangle.getPerimeter()) maxOfIsosceles=thisTriangle.getArea();
             }
-            else if(triangles[i].typeTriangle==TypeTriangle.RECTANGULAR)
+            else if(thisTriangle.getTypeTriangle()==TypeTriangle.RECTANGULAR)
             {
-                if(maxOfRectangular<triangles[i].getPerimeter()) maxOfRectangular=triangles[i].getArea();
+                if(maxOfRectangular<thisTriangle.getPerimeter()) maxOfRectangular=thisTriangle.getArea();
             }
-            else if(maxOfArbitrary<triangles[i].getPerimeter()) maxOfArbitrary=triangles[i].getArea();
+            else if(maxOfArbitrary<thisTriangle.getPerimeter()) maxOfArbitrary=thisTriangle.getArea();
         }
 
-        for(int i=0; i< triangles.length; i++)
+        for(Triangle thisTriangle:triangles)
         {
-            if(triangles[i].typeTriangle==TypeTriangle.EQUILATERAL)
+            if(thisTriangle.getTypeTriangle()==TypeTriangle.EQUILATERAL)
             {
-                if(maxOfEquilateral==triangles[i].getPerimeter())
+                if(maxOfEquilateral==thisTriangle.getPerimeter())
                 {
                     System.out.println("Maximum Perimeter in Equilateral:");
-                    System.out.println(triangles[i]);
+                    System.out.println(thisTriangle);
                 }
             }
-            else if(triangles[i].typeTriangle==TypeTriangle.ISOSCELES)
+            else if(thisTriangle.getTypeTriangle()==TypeTriangle.ISOSCELES || thisTriangle.isIsosceles())
             {
-                if(maxOfIsosceles==triangles[i].getPerimeter())
+                if(maxOfIsosceles==thisTriangle.getPerimeter())
                 {
                     System.out.println("Maximum Perimeter in Isosceles:");
-                    System.out.println(triangles[i]);
+                    System.out.println(thisTriangle);
                 }
             }
-            else if(triangles[i].typeTriangle==TypeTriangle.RECTANGULAR)
+            else if(thisTriangle.getTypeTriangle()==TypeTriangle.RECTANGULAR)
             {
-                if(maxOfRectangular==triangles[i].getPerimeter())
+                if(maxOfRectangular==thisTriangle.getPerimeter())
                 {
                     System.out.println("Maximum Perimeter in Rectangular:");
-                    System.out.println(triangles[i]);
+                    System.out.println(thisTriangle);
                 }
             }
-            else if(maxOfArbitrary==triangles[i].getPerimeter())
+            else if(maxOfArbitrary==thisTriangle.getPerimeter())
             {
                 System.out.println("Maximum Perimeter in Arbitrary:");
-                System.out.println(triangles[i]);
+                System.out.println(thisTriangle);
             }
         }
     }
 
     private static int getNumberOfTriangles()
     {
-        int numberOfTringles=0;
+        int numberOfTriangles;
         System.out.print("Number of Triangles: ");
         Scanner in = new Scanner(System.in);
-        numberOfTringles = in.nextInt();
+        numberOfTriangles = in.nextInt();
 
-        if(numberOfTringles<=0)throw new RuntimeException();
-        return numberOfTringles;
+        if(numberOfTriangles<=0)throw new RuntimeException();
+        return numberOfTriangles;
     }
 
     private static void printNumberOfTypesOfTriangles(Triangle[] triangles)
     {
-        int numOfEqulateral = 0;
+        int numOfEquilateral= 0;
         int numOfIsosceles = 0;
         int numOfRectangular = 0;
         int numOfArbitrary = 0;
 
-        for (int i = 0; i < triangles.length; i++)
+        for (Triangle thisTriangle:triangles)
         {
-            TypeTriangle typeTriangle = triangles[i].defineType();
-            if (typeTriangle == TypeTriangle.EQUILATERAL) numOfEqulateral++;
-            else if (typeTriangle == TypeTriangle.ISOSCELES) numOfIsosceles++;
-            else if (typeTriangle == TypeTriangle.RECTANGULAR) numOfRectangular++;
+            TypeTriangle typeTriangle = thisTriangle.defineType();
+            if (typeTriangle == TypeTriangle.EQUILATERAL) numOfEquilateral++;
+            else if (typeTriangle == TypeTriangle.ISOSCELES || thisTriangle.isIsosceles) numOfIsosceles++;
+            if (typeTriangle == TypeTriangle.RECTANGULAR) numOfRectangular++;
             else if (typeTriangle == TypeTriangle.ARBITRARY) numOfArbitrary++;
         }
         System.out.println("Types of Triangles: ");
-        System.out.println("Equlateral - " + numOfEqulateral);
+        System.out.println("Equilateral - " + numOfEquilateral);
         System.out.println("Isosceles - " + numOfIsosceles);
         System.out.println("Rectangular - " + numOfRectangular);
         System.out.println("Arbitrary - " + numOfArbitrary);
     }
-
     private static void fillTriangles(Triangle[] triangles)
     {
-        System.out.print("Choose the way of filling(Handle-0, Random-1):");
+        System.out.print("Choose the way of filling(0-Handle, 1-Random):");
         Scanner in = new Scanner(System.in);
         int val = in.nextInt();
-if(val==0) fillHandle(triangles);
-else if(val==1)fillRandom(triangles);
-else throw new RuntimeException();
+        if(val==0) fillHandle(triangles);
+        else if(val==1)fillRandom(triangles);
+        else throw new RuntimeException();
+    }
+
+    private static void printAllTriangles(Triangle[] triangles)
+    {
+        System.out.println("Print All Triangles(1-Y, 0-N)?");
+        Scanner in = new Scanner(System.in);
+        int val = in.nextInt();
+        if(val==1)
+        {
+            for(int i=0; i<triangles.length; i++)
+            {
+                System.out.println((i+1)+ " Triangle:");
+                System.out.println(triangles[i]);
+            }
+        }
+        else throw new RuntimeException();
     }
 
     public static void main(String[] args)
     {
-        Triangle[] triangles=new Triangle[getNumberOfTriangles()];
+        int numberOfTriangles=getNumberOfTriangles();
+        Triangle[] triangles=new Triangle[numberOfTriangles];
         fillTriangles(triangles);
 
         printNumberOfTypesOfTriangles(triangles);
+        printAllTriangles(triangles);
 
         printMaxArea(triangles);
         printMinArea(triangles);
